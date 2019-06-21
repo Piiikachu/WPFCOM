3e320b2e7a6effec36bfe87020a0c5340cba5ea9
Piiikachu <314475440@qq.com>
2019-06-21 13:49:55 +0800

    init

---
+++ WpfApp1/Transform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Transform
+    {
+        public static double toPressure(double current)
+        {
+            if (current < 3.98 || current > 20)
+            {
+                throw new Exception("Input current out of range");
+            }
+            if (current >= 3.98 && current < 5.6)
+            {
+                return 10 * (current - 3.98) / 1.62;
+            }
+            else
+            {
+                return 10 + 10 / 1.6 * (current - 5.6);
+            }
+        }
+
+        public static double toDec(string hexNumber)
+        {
+            return Convert.ToInt32(hexNumber, 16) / 100;
+        }
+    }
+}
